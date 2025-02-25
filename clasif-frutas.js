b90701e83c83bf7198bda5c39e6edc8ad71965a4
Randy DEVF <noiseck1796@gmail.com>
2025-02-25 12:28:37 -0600

    creacion de un arreglo con distintas frutas, objeto para poder clasificar y otro arreglo con la clasificacion de las frutas

---
+++ clasif-frutas.js
@@ -0,0 +1,21 @@
+// Arreglo con diferentes frutas
+let frutas = [
+    "Limón", "Naranja", "Piña", "Fresa",  // Ácidas
+    "Manzana", "Mango", "Ciruela",        // Semiácidas
+    "Plátano", "Uva", "Higo", "Sandía",   // Dulces
+    "Limón", "Manzana", "Piña", "Mango", "Higo", "Uva", "Naranja"
+];
+
+// Objeto para contar la cantidad de cada tipo de fruta
+let conteoFrutas = {
+    acidas: 0,
+    semiacidas: 0,
+    dulces: 0
+};
+
+// Clasificación de frutas según su acidez
+let clasificacion = {
+    acidas: ["Limón", "Naranja", "Piña", "Fresa"],
+    semiacidas: ["Manzana", "Mango", "Ciruela"],
+    dulces: ["Plátano", "Uva", "Higo", "Sandía"]
+};

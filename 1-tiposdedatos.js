87d2a98fcbfd8c65a4df7c645335d0d31d8c4580
Randy DEVF <noiseck1796@gmail.com>
2025-02-25 11:16:58 -0600

    se introducen los datos en cosnola para que se puedan imprimir

--- 1-tiposdedatos.js
+++ 1-tiposdedatos.js
@@ -8,3 +8,14 @@ let textoVacio = ''; // String vacío
 let nulo = null; // Null
 let indefinido; // Undefined
 let booleano2 = false; // Boolean
+
+// Imprimir los valores y sus tipos
+console.log("Número 1:", numero1, typeof numero1);
+console.log("Texto:", texto, typeof texto);
+console.log("Número 2:", numero2, typeof numero2);
+console.log("Booleano 1:", booleano1, typeof booleano1);
+console.log("Número 3:", numero3, typeof numero3);
+console.log("Texto vacío:", textoVacio, typeof textoVacio);
+console.log("Nulo:", nulo, typeof nulo); // typeof null devuelve 'object' (quirk de JS)
+console.log("Indefinido:", indefinido, typeof indefinido);
+console.log("Booleano 2:", booleano2, typeof booleano2);

80a1dd31af20076ce5a76d4b256c0e1223bbcad9
Randy DEVF <noiseck1796@gmail.com>
2025-02-25 11:15:19 -0600

    se agregan los tipos de datos

---
+++ 1-tiposdedatos.js
@@ -0,0 +1,10 @@
+// Valores proporcionados
+let numero1 = 42; // Number
+let texto = 'Veinticinco'; // String
+let numero2 = -666; // Number
+let booleano1 = true; // Boolean
+let numero3 = 0; // Number
+let textoVacio = ''; // String vacío
+let nulo = null; // Null
+let indefinido; // Undefined
+let booleano2 = false; // Boolean
